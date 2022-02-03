5bd94dc165d56ffcfb4f6e40bb6cd6833e119073
kim-dongho <dongho_k@naver.com>
2022-02-03 16:53:49 +0900

    react-ts props

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Circle from './Circle';
+
+const App = () => {
+  return (
+    <div>
+      <Circle bgColor='teal' borderColor='yellow' />
+      <Circle bgColor='tomato' text='im here' />
+    </div>
+  );
+};
+
+export default App;
